32fe40e9351f310b3339d4d62d5f814ff65365ba
张敦勇 <dunyong.zhang@igg.com>
2023-08-09 14:23:01 +0800

    1

--- dict.py
+++ dict.py
@@ -1,16 +1,32 @@
 import stardict
 import sys
 import io
+import os
 
 sys.stdout.reconfigure(encoding='utf-8')
 
-word = len(sys.argv) > 1 and sys.argv[1] or "move"
+# print('getcwd:      ', os.getcwd())
+# print('__file__:    ', __file__)
 
-x = stardict.open_dict("xx.db")
+path = os.path.dirname(__file__)
+word = ""
+if len(sys.argv) <= 1:
+	word = "hello world"
+else:
+	for x in range(1, len(sys.argv)):
+		if word != "":
+			word += " "
+		word += sys.argv[x]
+
+x = stardict.open_dict(path + "/xx.db")
 queryResult = x.query(word)
+
+if queryResult == None:
+	print("未找到【{}】".format(word))
+	exit(0)
+
 
 print("\n")
 print("单词【{}】音标【{}】 柯林斯: {} 牛津核心: {} 分类：{}".format(queryResult["word"], queryResult["phonetic"], queryResult["collins"], queryResult["oxford"], queryResult["tag"]))
-print("\n")
 print(queryResult["definition"])
 print(queryResult["translation"])
